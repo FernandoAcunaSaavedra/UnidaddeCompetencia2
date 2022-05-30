26b9782e4922bdbf28f0d8a660fc5f17c3ce6cc0
FernandoAcunaSaavedra <fernandoacunasaavedra@gmail.com>
2022-05-30 13:33:09 -0500

    Semana07 unidad de competencia2

---
+++ gestion_archivos.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 30 12:01:32 2022
+
+@author: ferna
+"""
+import os
+def crear_archivo(nombre,contenido):
+    archivo= open(nombre,"wt")
+    archivo.write(contenido)
+    archivo.close()
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+def eliminar_archivo(nombre):
+    os.remove(nombre)
+
+def agregar_contenido_archivo(nombre,contenido):
+    archivo = open(nombre, "at")
+    archivo.write("\n" + contenido)
+    archivo.close()
+
+def leer_archivo(nombre):
+    archivo = open(nombre,"rt", encoding='utf8')
+    contenido = archivo.read()
+    return contenido
